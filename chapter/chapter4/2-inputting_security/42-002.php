e13ef7073756a71bc0375f6dfa90a750931ee80a
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-28 15:40:22 +0900

    xss

---
+++ chapter/chapter4/2-inputting_security/42-002.php
@@ -0,0 +1,11 @@
+<body>
+    <?php
+    $p = $GET['p'];
+    if(preg_match('[0-9]', $p) === FALSE) {
+        die('整数を入れてください');
+    }
+    echo $p;
+    ?>
+</body>
+
+chapter4/2-inputting_security/42-002.php?p=1%00<spript>alert('XSS')</script>
