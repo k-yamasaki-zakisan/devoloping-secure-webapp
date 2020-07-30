ce5fb4355b1241c86b8caaaa31a44d3d4d49a21d
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-31 08:51:58 +0900

    ファイルアクセス攻撃、ルートの固定化

--- chapter/chapter4/10-file_access/4a-001b.php
+++ chapter/chapter4/10-file_access/4a-001b.php
@@ -3,6 +3,10 @@
     // 重要ファイルにアクセスされないようにルートの固定化
     define('TMPLDIR', '/var/www/html/chapter4/10-file_access/');
     ＄tmp1 = basename(filter_input(INPUT_GET, 'template'));
+    // 正規表現でファイル名は英数字のみにする方法もあり
+    if (preg_match('/\A[a-z0-9]+\z/ui', $tmp1 !==1)) {
+        die('templateは英数字のみで指定できます')
+    }
 ?>
 
 <body>
