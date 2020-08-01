fbaa0ebc9ae3472e4395aaa6561819c74d3dd6d8
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-01 13:05:59 +0900

    ファイルインクルード攻撃

---
+++ chapter/chapter4/13_file_include/4d-001.php
@@ -0,0 +1,10 @@
+<body>
+<?php
+$header = $_GET['header'];
+require_once($header . '.php');
+?>
+本文【省略】
+</body>
+
+<!-- http://localhost:8090/chapter4/13_file_include/4d-001.php?header=spring -->
+<!-- http://localhost:8090/chapter4/13_file_include/4d-001.php?header=../../../../../etc/hosts%00 -->
